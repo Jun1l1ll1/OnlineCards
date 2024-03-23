d4277eef29035c2597735753bc3e9f3e9865b8ed
Jun1l1ll1 <jujoa007@osloskolen.no>
2024-03-23 16:34:41 +0100

    player boxes added

--- script.js
+++ script.js
@@ -1,21 +1,23 @@
 
-
-
 function join_game() {
     let join_code = document.getElementById("join_inp").value.toUpperCase();
-    if (join_code.length == 6) {
+    let name = document.getElementById("name_inp").value;
+    if (join_code.length == 6 && name != "") {
         console.log(join_code);
         //TODO koble til et spill
     }
 }
 
+
+
 function create_game() {
-    let join_code = generate_ID();
-    //TODO lag spill i databasen (firebase?)
-    window.location.href = "/game.html?game="+join_code; 
+    let name = document.getElementById("name_inp").value;
+    if (name != "") {
+        let join_code = generate_ID();
+        //TODO lag spill i databasen og legg inn spilleren med navn (firebase?) (+ pass på at ikke flere spillere har samme navn)
+        window.location.href = "/game.html?game="+join_code+",name="+name; 
+    }
 }
-
-
 
 function generate_ID() {
     const DATA = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
@@ -30,6 +32,32 @@ function generate_ID() {
 
 
 
+
+
+
 function create_playerboxes() {
-    
+    let container_html = document.getElementById("playerboxes");
+    container_html.innerHTML = "";
+    for (let i = 0; i < 10; i++) {
+        //TODO legg til klassen "is_me" om spilleren er denne brukeren
+
+        container_html.innerHTML += `
+        <div class="playerbox br_20">
+            <button>...</button>
+            <div class="shown_cards">
+
+            </div>
+            <div class="name_and_amount">
+                <h5>NAVN</h5>
+                <div>
+                    <img src="" alt="" />
+                    <span>#</span>
+                </div>
+            </div>
+            <div class="last_played">
+                <img src="" alt="">
+            </div>
+        </div>
+        `;
+    }
 }
